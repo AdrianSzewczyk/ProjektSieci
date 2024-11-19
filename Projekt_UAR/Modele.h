d354e1bff91b177993e9742cfe28bc1a0c808928
chaosRobe <dominik.panasz@outlook.com>
2024-11-19 19:10:50 +0100

    Kompozycja - Test

--- Projekt_UAR/Modele.h
+++ Projekt_UAR/Modele.h
@@ -1,1 +1,32 @@
 #pragma once
+#include <vector>
+#include <iostream>
+#include "Symulacja.h"
+class Model
+{
+public:
+	virtual void operacja(Symulacja& sym) const = 0;
+};
+
+class Model_ARX : public Model
+{
+private:
+	std::vector<Model*> m_modele;
+public:
+	void dodaj(Model* mod);
+	void operacja(Symulacja& sym) const override;
+	void wywolaj(Symulacja& sym);
+};
+
+class Model_PID : public Model
+{
+public:
+	void operacja(Symulacja& sym) const override;
+};
+
+class Model_Generator : public Model
+{
+
+public:
+	void operacja(Symulacja& sym) const override;
+};
--- Projekt_UAR/main.cpp
+++ Projekt_UAR/main.cpp
@@ -1,29 +1,18 @@
-class Symulacja 
-{
-
-};
-
-class Model 
-{
-
-};
-
-class Model_ARX : public Model
-{
-
-};
-
-class Model_PID : public Model
-{
-
-};
-
-class Model_Generator : public Model
-{
-
-};
+#include <iostream>
+#include "Symulacja.h"
+#include "Modele.h"
 
 int main()
 {
-
+	Model_ARX model_arx;
+	Model_Generator model_gen;
+	Model_PID model_pid;
+	model_arx.dodaj(&model_gen);
+	model_arx.dodaj(&model_pid);
+	Symulacja sym;
+	std::cout << sym;
+	std::cout << std::endl;
+	model_arx.wywolaj(sym);
+	std::cout << sym;
+	std::cout << std::endl;
 }
