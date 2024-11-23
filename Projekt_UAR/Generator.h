00470e2c35e34c1fea427dd2e03bfc219d1e66d6
chaosRobe <dominik.panasz@outlook.com>
2024-11-24 00:55:39 +0100

    Generator - funkcjonalnosc

--- Projekt_UAR/Generator.h
+++ Projekt_UAR/Generator.h
@@ -1,1 +1,47 @@
 #pragma once
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <iostream>
+
+class Generator 
+{
+private:
+	int T = 2;
+	int iteration = 0;
+	double Amp = 1;
+public:
+	Generator() {};
+	void set_T(int T_val);
+	void set_Amp(double Amp_val);
+	void Iterate();
+	int get_T();
+	double get_Amp();
+	int get_Iteration();
+	virtual double Generate() { return 0; };
+};
+
+class Generator_SKOK : public Generator
+{
+private:
+
+public:
+	double Generate() override;
+};
+
+class Generator_SIN : public Generator
+{
+private:
+
+public:
+	double Generate() override;
+};
+
+class Generator_SYG : public Generator
+{
+private:
+	double fill = 0;
+public:
+	void set_fill(double fill_val);
+	double get_fill();
+	double Generate() override;
+};
--- Projekt_UAR/main.cpp
+++ Projekt_UAR/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
-
+#include "Generator.h"
 int main()
 {
-	
+	Generator_SKOK genSKOK;
+	Generator_SIN genSIN;
+	Generator_SYG genSYG;
+
+	double amp = 1;
+	int valT = 50;
+	double fill = 0.9;
+
+	genSKOK.set_Amp(amp);
+	genSIN.set_Amp(amp);
+	genSYG.set_Amp(amp);
+
+	genSKOK.set_T(valT);
+	genSIN.set_T(valT);
+	genSYG.set_T(valT);
+
+	genSYG.set_fill(fill);
+
+	for (int i = 0; i < 100; i++)
+	{
+		std::cout <<"SKOK: "<< genSKOK.Generate() << std::endl;
+	}
+
+	for (int i = 0; i < 100; i++)
+	{
+		std::cout << "SIN: " << genSIN.Generate() << std::endl;
+	}
+
+	for (int i = 0; i < 100; i++)
+	{
+		std::cout << "SYG: " << genSYG.Generate() << std::endl;
+	}
 }
