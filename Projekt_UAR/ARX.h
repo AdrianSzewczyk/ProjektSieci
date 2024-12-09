1a7473d5f6597053a6311ea78a4cebf0e724a939
chaosRobe <dominik.panasz@outlook.com>
2024-12-09 20:22:05 +0100

    Testy - Arx

--- Projekt_UAR/ARX.h
+++ Projekt_UAR/ARX.h
@@ -7,17 +7,20 @@ class model_ARX
 private:
 	int iteration = 0;
 	double y_output = 0;
+	int input_buffer_size = 0;
 	std::vector<double> values_A;
 	std::vector<double> values_B;
+	std::deque<double> buffer_input;
 	std::deque<double> buffer_error_u;
 	std::deque<double> buffer_output_y;
 public:
 	void Iterate();
 	double Simulate(double pid_val);
-	model_ARX(std::vector<double> A, std::vector<double> B)
+	model_ARX(std::vector<double> A, std::vector<double> B, int inp)
 	{
 		values_A = A;
 		values_B = B;
+		input_buffer_size = inp;
 	}
 	model_ARX() = delete;
 };
