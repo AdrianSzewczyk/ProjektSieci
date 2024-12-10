09c46edf9b594255a70118b470cf5057b0110ff2
chaosRobe <dominik.panasz@outlook.com>
2024-12-10 18:33:31 +0100

    Symulator - setery

--- Projekt_UAR/Symulator.cpp
+++ Projekt_UAR/Symulator.cpp
@@ -1,9 +1,31 @@
 #include "Symulator.h"
 
-void Symulator::set_generator_type(typ_generatora typ) {};
-void Symulator::set_arx(std::vector<double> A, std::vector<double> B, int opoznienie, bool zaklocenia) {};
-void Symulator::set_pid(double proporcja, double calka, double rozniczka) {};
-void Symulator::set_gen(double amplituda, int okres, int zapelnienie) {};
-void Symulator::iterate() {};
+void Symulator::set_generator_type(typ_generatora typ) 
+{
+	typ_gen = typ;
+};
+void Symulator::set_arx(std::vector<double> A, std::vector<double> B, int opoznienie, bool zaklocenia) 
+{
+	arx.set_vector_A(A);
+	arx.set_vector_B(B);
+	arx.set_latency(opoznienie);
+	arx.set_disruption_status(zaklocenia);
+};
+void Symulator::set_pid(double proporcja, double calka, double rozniczka) 
+{
+	pid.set_k(proporcja);
+	pid.set_Ti(calka);
+	pid.set_Td(rozniczka);
+};
+void Symulator::set_gen(double amplituda, int okres, int zapelnienie) 
+{
+	gen.set_Amp(amplituda);
+	gen.set_T(okres);
+	gen.set_fill(zapelnienie);
+};
+void Symulator::iterate() 
+{
+	iteration++;
+};
 
 double Symulator::simulate() {};
