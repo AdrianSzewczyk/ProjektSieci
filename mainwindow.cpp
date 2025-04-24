04b581b38fbe37711710aabd280a9f00b387210f
1jw310844 <jw310844@student.polsl.pl>
2025-04-24 22:47:40 +0200

    MessageBoxy

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -773,7 +773,7 @@ void MainWindow::siec_connected(){
     serwerPołączony=true;
     ui->statusPolaczony->setText("Połączony");
     ui->statusPolaczony->setStyleSheet("QLabel { color : green; }");
-
+    QMessageBox::information(this,"Połączenie","Połączono z serwerem!!!");
 }
 void MainWindow::siec_disconnected(){
 
@@ -782,7 +782,7 @@ void MainWindow::siec_disconnected(){
     serwerPołączony=false;
     ui->statusPolaczony->setText("Niepołączony");
     ui->statusPolaczony->setStyleSheet("QLabel { color : red; }");
-
+    QMessageBox::information(this,"Połączenie","Rozłączono!!!");
 
 }
 void MainWindow::siec_stateChanged(QAbstractSocket::SocketState state){
